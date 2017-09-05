c7f655f82438ed3a9d312e5054a07d4bea0a138e
Ravindra Nadh <ravi.yelugula@gmail.com>
2017-09-05 22:26:20 +0530

    Creating CS Project
    this is a new project - capstone for Great Lakes -- Customer Satisfaction analysis

---
+++ Customer_Satisfaction_Capstone/feature engineering.R
@@ -0,0 +1,9 @@
+### Focus       		: Feature Engineering on Customer Satisfaction dataset           
+### CreatedOn   		: 05-sep-2017 
+### Author      		: Ravindranadh 
+### Location    		: GreatLakes Institute of Managment, Chennai
+### ProjectName 		:
+### Latest Revision : V 1.0
+
+
+hai 
