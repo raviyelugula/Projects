83c8d1453dadb6ba01e1a6a7dd11107f9d7d08d2
Ravindra Nadh <ravi.yelugula@gmail.com>
2017-09-06 21:17:00 +0530

    Sep 6th - Commit 4
    Added correlation plots and PCA reduction code

--- Customer_Satisfaction_Capstone/feature engineering.R
+++ Customer_Satisfaction_Capstone/feature engineering.R
@@ -7,9 +7,9 @@
 
 ### data set Reading
 require(readxl)
-dataset = read_excel('Workingdata.xlsx')
-names(dataset)
-colnames(dataset) = c(colnames(dataset[,1:17]),'Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10',colnames(dataset[,28]),'Remarks')
+Dataset = read_excel('Workingdata.xlsx')
+names(Dataset)
+colnames(Dataset) = c(colnames(Dataset[,1:17]),'Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10',colnames(Dataset[,28]),'Remarks')
 
 ### Missing value Analysis
 Missing_data_Check <- function(data_set){
@@ -21,7 +21,7 @@ Missing_data_Check <- function(data_set){
   return( Total_NonData )
 }
 
-Missing_Values = Missing_data_Check(dataset)
+Missing_Values = Missing_data_Check(Dataset)
 MissingValuesPlot = barplot(Missing_Values,col = 'gray',
                             main ='Missing Values across all attributes',
                             xlab = '',ylab = "Missing count",xaxt = 'n')
@@ -38,29 +38,29 @@ text(x = MissingValuesPlotQ, y = Missing_Values[18:27],
 axis(1, at=MissingValuesPlotQ, labels=names(Missing_Values[18:27]), 
      tick=FALSE, las=2, line=-0.5, cex.axis=0.5)
 
-missing_all = subset(dataset, is.na(dataset$Q1) &
-                       is.na(dataset$Q2)&
-                       is.na(dataset$Q3)&
-                       is.na(dataset$Q4)&
-                       is.na(dataset$Q5)&
-                       is.na(dataset$Q6)&
-                       is.na(dataset$Q7)&
-                       is.na(dataset$Q8)&
-                       is.na(dataset$Q9)&
-                       is.na(dataset$Q10))
+missing_all = subset(Dataset, is.na(Dataset$Q1) &
+                       is.na(Dataset$Q2)&
+                       is.na(Dataset$Q3)&
+                       is.na(Dataset$Q4)&
+                       is.na(Dataset$Q5)&
+                       is.na(Dataset$Q6)&
+                       is.na(Dataset$Q7)&
+                       is.na(Dataset$Q8)&
+                       is.na(Dataset$Q9)&
+                       is.na(Dataset$Q10))
 
-missing_anyone = subset(dataset, is.na(dataset$Q1) |
-                          is.na(dataset$Q2)|
-                          is.na(dataset$Q3)|
-                          is.na(dataset$Q4)|
-                          is.na(dataset$Q5)|
-                          is.na(dataset$Q6)|
-                          is.na(dataset$Q7)|
-                          is.na(dataset$Q8)|
-                          is.na(dataset$Q9)|
-                          is.na(dataset$Q10))
+missing_anyone = subset(Dataset, is.na(Dataset$Q1) |
+                          is.na(Dataset$Q2)|
+                          is.na(Dataset$Q3)|
+                          is.na(Dataset$Q4)|
+                          is.na(Dataset$Q5)|
+                          is.na(Dataset$Q6)|
+                          is.na(Dataset$Q7)|
+                          is.na(Dataset$Q8)|
+                          is.na(Dataset$Q9)|
+                          is.na(Dataset$Q10))
 
-Qdataset = dataset[,18:27]
+Qdataset = Dataset[,18:27]
 for(i in 1:10){
   dataset_name = paste('missing_Q',i%%11,sep='')
   temp = subset(Qdataset, is.na(Qdataset[i%%11])&
@@ -77,28 +77,22 @@ for(i in 1:10){
 }
 rm(temp)
 
-# names = c('missing_all','missing_anyone','missing_Q1','missing_Q2','missing_Q3',
-#           'missing_Q4','missing_Q5','missing_Q6','missing_Q7','missing_Q8',
-#           'missing_Q9','missing_Q10')
-# for(i in 1:12){
-#   j = nrow(noquote(names[i]))
-# }
-
-
-Qdataset_no_missing=Qdataset[!apply(Qdataset, 1, function(x) any(x=="" | is.na(x))),] 
-require(psych)
-colnames(Qdataset_no_missing) = c('Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10')
-pca = principal(Qdataset_no_missing,nfactors = ncol(Qdataset_no_missing),rotate = 'none')
-pca
-
-pca_1 = principal(Qdataset_no_missing, nfactors = 1, rotate = 'none')
-pca_1
-
-pca_rotated = principal(Qdataset_no_missing, nfactors = 1, rotate = 'varimax')
-pca_rotated
-
+Names = c('missing_all','missing_anyone','missing_Q1','missing_Q2','missing_Q3',
+          'missing_Q4','missing_Q5','missing_Q6','missing_Q7','missing_Q8',
+          'missing_Q9','missing_Q10')
+Missing_Counts = c(nrow(missing_all),nrow(missing_anyone),nrow(missing_Q1),nrow(missing_Q2),nrow(missing_Q3),
+                   nrow(missing_Q4),nrow(missing_Q5),nrow(missing_Q6),nrow(missing_Q7),nrow(missing_Q8),
+                   nrow(missing_Q9),nrow(missing_Q10))
+names(Missing_Counts) = Names
+MissingCountPlot = barplot(Missing_Counts,col = 'gray',
+                            main ='Missing Value trend',
+                            xlab = '',ylab = "Missing count",xaxt = 'n')
+text(x = MissingCountPlot, y = Missing_Counts,
+     label = Missing_Counts,col = "red", cex = 0.8,font = 2)
+axis(1, at=MissingCountPlot, labels=names(Missing_Counts), 
+     tick=FALSE, las=2, line=-0.5, cex.axis=0.8)
+### Correlation Analysis on Questions data
 Correlation=cor(Qdataset_no_missing)
-
 require(corrplot)
 require(RColorBrewer)
 
@@ -106,4 +100,17 @@ corrplot(Correlation, type="upper",
          method = 'circle' ,order="hclust", add = F,
          col=brewer.pal(n=4, name="RdBu"),  
          outline = T)
+
+### Factor Analysis on Non missing data set of 10 Questions
+Qdataset_no_missing=Qdataset[!apply(Qdataset, 1, function(x) any(x=="" | is.na(x))),] 
+require(psych)
+pca = principal(Qdataset_no_missing,nfactors = ncol(Qdataset_no_missing),rotate = 'none')
+pca
+
+pca_reduced = principal(Qdataset_no_missing, nfactors = 1, rotate = 'none')
+pca_reduced
+
+pca_rotated = principal(Qdataset_no_missing, nfactors = 1, rotate = 'varimax')
+pca_rotated
+
 
