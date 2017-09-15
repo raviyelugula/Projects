758ccfacdfefd9c90deeeff28cf53ebe1fba44e8
Ravindra Nadh <ravi.yelugula@gmail.com>
2017-09-15 11:51:15 +0530

    Added StemDocument

--- Twitter HashTag Analysis/Twitter_HashTag_Analysis.R
+++ Twitter HashTag Analysis/Twitter_HashTag_Analysis.R
@@ -9,21 +9,24 @@ setup_twitter_oauth(
 getCurRateLimitInfo(resources='search')
 
 ThursdayThoughts= searchTwitter(searchString ='#ThursdayThoughts',
-                              n=2000 , lang = 'en') 
+                                n=2000 , lang = 'en') 
 # last ran at 8:08 PM 14-Sep-2017
 ThursdayThoughts_RT_Removed = strip_retweets(ThursdayThoughts,
                                              strip_manual = T,
                                              strip_mt = T)
 TT_dataframe = twListToDF(ThursdayThoughts)
 write.csv(TT_dataframe,'TT_dataframe.csv')
-Tweets = TT_dataframe$text
 
+# TT_dataframe = read.csv(file = 'Data.csv', header = T)
+# TT_dataframe = TT_dataframe[-1]
+Tweets = as.character(TT_dataframe$text)
 require(tm)
 Tweets_Corpus = Corpus(VectorSource(Tweets))
 
 removeUsernameInRT <- function(x) str_replace_all(x,"(?<=@)[^\\s:]+",'')
 Tweets_Corpus = tm_map(Tweets_Corpus, content_transformer(removeUsernameInRT))
 
+require(stringr)
 removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
 Tweets_Corpus = tm_map(Tweets_Corpus, content_transformer(removeURL))
 
@@ -37,17 +40,22 @@ removeSingle <- function(x) gsub(" . ", " ", x)
 Tweets_Corpus = tm_map(Tweets_Corpus, content_transformer(removeSingle))
 Tweets_Corpus = tm_map(Tweets_Corpus,stripWhitespace)
 Tweets_Corpus = tm_map(Tweets_Corpus,content_transformer(tolower))
+Tweets_Corpus = tm_map(Tweets_Corpus, content_transformer(replace_abbreviation))
 
 Tweets_Corpus_Filtered = Tweets_Corpus
 Tweets_Corpus<-tm_map(Tweets_Corpus, stemDocument,language = 'english')
-stemCompletionuserDefined <- function(x,dictionary) {
-  x = unlist(strsplit(as.character(x)," "))
-  x = x[x !=""]
-  x = stemCompletion(x, dictionary = dictionary)
-  x = paste(x, sep="", collapse=" ")
-  PlainTextDocument(stripWhitespace(x))
-}
-Tweets_Corpus = lapply(Tweets_Corpus, stemCompletionuserDefined, dictionary=Tweets_Corpus_Filtered)
+# stemCompletionuserDefined <- function(x,dictionary) {
+#   x = unlist(strsplit(as.character(x)," "))
+#   x = x[x !=""]
+#   x = stemCompletion(x, dictionary = dictionary)
+#   x = paste(x, sep="", collapse=" ")
+#   PlainTextDocument(stripWhitespace(x))
+# }
+# Tweets_Corpus = lapply(Tweets_Corpus, stemCompletionuserDefined, dictionary=Tweets_Corpus_Filtered)
+inspect(Tweets_Corpus[1:5])
+Tweets_Corpus <- tm_map(Tweets_Corpus, stemCompletion, dictionary=Tweets_Corpus_Filtered)
+inspect(Tweets_Corpus[1:5])
+
 Tweets_Corpus = Corpus(VectorSource(Tweets_Corpus))
 backup_Tweets_Corpus = Tweets_Corpus
 
@@ -66,6 +74,3 @@ frequency = freq_terms(TT_dataframe$text,
                        top = 25,
                        at.least = 1000,
                        stopwords =stopwords('english'))
-
-
-
