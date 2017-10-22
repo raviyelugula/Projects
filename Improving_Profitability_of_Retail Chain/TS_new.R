8155f7810e0afdfe84026a054cc276eb57766ec1
Ravindra Nadh <ravi.yelugula@gmail.com>
2017-10-22 20:39:18 +0530

    22nd Oct

--- Improving_Profitability_of_Retail Chain/TS_new.R
+++ Improving_Profitability_of_Retail Chain/TS_new.R
@@ -1,16 +1,9 @@
-data = read.csv(file = 'C:/Users/ravin/Documents/GitHub/Projects/Improving_Profitability_of_Retail Chain/work1.csv')
-names(data)
-.arg1 = sum(data$SPEND)
-a <- rep(1, length(.arg1))
-a[findpeaks(.arg1,
-            threshold=quantile(.arg1,.005)
-            ,sortstr=FALSE)[,2]]=0
-a
+str(data.work)
+sapply(sapply(data.work,length(unique)),length)
+count.fields(unique(data.work$STORE_NUM))
 
-quantile(.arg1,.38)
-quantile(c(1,2,3,4,5,6,7,8,9,10),.99)
+for(i in 1:25)
+print(c(nrow(unique(data.work[i])),names(data.work[i])))
 
 
-
-
-
+trimws(names(data.work[i]))
