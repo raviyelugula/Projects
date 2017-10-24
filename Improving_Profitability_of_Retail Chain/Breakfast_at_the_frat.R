365893e714886426d8dc87f5ff3c3562913af1bf
Ravindra Nadh <ravi.yelugula@gmail.com>
2017-10-25 01:35:42 +0530

    Read_Handled_Missingdata

--- Improving_Profitability_of_Retail Chain/Breakfast_at_the_frat.R
+++ Improving_Profitability_of_Retail Chain/Breakfast_at_the_frat.R
@@ -3,8 +3,9 @@ require(dplyr)
 excel_sheets('dunnhumby - Breakfast at the Frat.xlsx')
 rawdata = read_excel(path = 'dunnhumby - Breakfast at the Frat.xlsx', sheet = 'dh Transaction Data')
 names(rawdata)
+rawStoreData = read_excel(path = 'dunnhumby - Breakfast at the Frat.xlsx', sheet = 'dh Store Lookup')
 rawdata = rawdata %>% 
-              left_join(read_excel(path = 'dunnhumby - Breakfast at the Frat.xlsx', sheet = 'dh Store Lookup'),
+              left_join(rawStoreData[which(!duplicated(rawStoreData$STORE_ID)),],
                         by = c('STORE_NUM'='STORE_ID')) %>%
               left_join(read_excel(path = 'dunnhumby - Breakfast at the Frat.xlsx', sheet = 'dh Products Lookup'),
                         by = 'UPC')
@@ -12,12 +13,41 @@ Missing_data_Check <- function(data_set){
   NA_Count = sapply(data_set,function(y) sum(length(which(is.na(y))))) 
   Null_Count = sapply(data_set,function(y) sum(length(which(is.null(y)))))
   Length0_Count = sapply(data_set,function(y) sum(length(which(length(y)==0))))
-  Empty_Count = sapply(data_set,function(y) sum(length(which(y==''))))
+  Empty_Count = sapply(data_set,function(y) {if(class(y) != 'Date') 
+                                              return(sum(length(which(y==''))))
+                                            else return(0)})
   Total_NonData = NA_Count+Null_Count+Length0_Count+Empty_Count
   return( Total_NonData )
 }
+rawdata$WEEK_END_DATE = as.Date(rawdata$WEEK_END_DATE)
 Missing_data_Check(rawdata)
 glimpse(rawdata)
- 
+non_baseprice_missing_rawdata = rawdata[,1:25][!apply(rawdata[,9], 1, function(x) any(is.na(x))),] 
+product_WeeklyAvg_basePrice = non_baseprice_missing_rawdata %>%
+                                select(WEEK_END_DATE,UPC,BASE_PRICE)%>%
+                                group_by(UPC,WEEK_END_DATE) %>%
+                                mutate(AVG_BASE_PRICE= round(mean(BASE_PRICE),2))
+product_WeeklyAvg_basePrice = unique(product_WeeklyAvg_basePrice[,-3])
+baseprice_missing_rawdata = rawdata[,1:25][apply(rawdata[,9], 1, function(x) any(is.na(x))),] 
+baseprice_missing_rawdata = baseprice_missing_rawdata %>%
+                              left_join(product_WeeklyAvg_basePrice,by=c("WEEK_END_DATE","UPC"))
+baseprice_missing_rawdata$BASE_PRICE = baseprice_missing_rawdata$AVG_BASE_PRICE
+rawdata_BasePrice = rbind(non_baseprice_missing_rawdata,baseprice_missing_rawdata[-26])
+rm(list = c('non_baseprice_missing_rawdata','product_WeeklyAvg_basePrice','baseprice_missing_rawdata'))
+non_price_missing_rawdata = rawdata_BasePrice[,1:25][!apply(rawdata_BasePrice[,8], 1, function(x) any(is.na(x))),] 
+product_WeeklyAvg_Price = non_price_missing_rawdata %>%
+                                select(WEEK_END_DATE,UPC,PRICE)%>%
+                                group_by(UPC,WEEK_END_DATE) %>%
+                                mutate(AVG_PRICE= round(mean(PRICE),2))
+product_WeeklyAvg_Price = unique(product_WeeklyAvg_Price[,-3])
+price_missing_rawdata = rawdata_BasePrice[,1:25][apply(rawdata_BasePrice[,8], 1, function(x) any(is.na(x))),] 
+price_missing_rawdata = price_missing_rawdata %>%
+                              left_join(product_WeeklyAvg_Price,by=c("WEEK_END_DATE","UPC"))
+price_missing_rawdata$PRICE = price_missing_rawdata$AVG_PRICE
+rawdata_BasePrice_Price = rbind(non_price_missing_rawdata,price_missing_rawdata[-26])
+rm(list = c('non_price_missing_rawdata','product_WeeklyAvg_Price','price_missing_rawdata','rawdata_BasePrice'))
+Missing_data_Check(rawdata_BasePrice_Price)
+
+
 
 
--- Improving_Profitability_of_Retail Chain/TS_new.R
+++
@@ -1,9 +0,0 @@
-str(data.work)
-sapply(sapply(data.work,length(unique)),length)
-count.fields(unique(data.work$STORE_NUM))
-
-for(i in 1:25)
-print(c(nrow(unique(data.work[i])),names(data.work[i])))
-
-
-trimws(names(data.work[i]))
