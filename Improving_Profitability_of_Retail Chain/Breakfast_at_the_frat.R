64830b10cb54e77779f8418f4f070aaf3fbd6442
Ravindra Nadh <ravi.yelugula@gmail.com>
2017-10-26 10:08:31 +0530

    Kmode added

--- Improving_Profitability_of_Retail Chain/Breakfast_at_the_frat.R
+++ Improving_Profitability_of_Retail Chain/Breakfast_at_the_frat.R
@@ -48,6 +48,29 @@ rawdata_BasePrice_Price = rbind(non_price_missing_rawdata,price_missing_rawdata[
 rm(list = c('non_price_missing_rawdata','product_WeeklyAvg_Price','price_missing_rawdata','rawdata_BasePrice'))
 Missing_data_Check(rawdata_BasePrice_Price)
 
+rawdata_BasePrice_Price$DISCOUNT_PRICE = rawdata_BasePrice_Price$BASE_PRICE - rawdata_BasePrice_Price$PRICE
+rawdata_BasePrice_Price$DISCOUNT_PERCENT = (rawdata_BasePrice_Price$DISCOUNT_PRICE/rawdata_BasePrice_Price$BASE_PRICE)*100
+rawdata_BasePrice_Price$DISCOUNT = ifelse(rawdata_BasePrice_Price$DISCOUNT_PRICE!=0,1,0)
+rawdata_factors = as.data.frame(unclass(rawdata_BasePrice_Price))
+rawdata_factors$FEATURE = as.factor(rawdata_factors$FEATURE)
+rawdata_factors$DISPLAY = as.factor(rawdata_factors$DISPLAY)
+rawdata_factors$TPR_ONLY = as.factor(rawdata_factors$TPR_ONLY)
+rawdata_factors$DISCOUNT = as.factor(rawdata_factors$DISCOUNT)
+rawdata_factors$MSA_CODE = as.factor(rawdata_factors$MSA_CODE)
+sapply(rawdata_factors,class)
+
+sapply(rawdata_factors[,c(10:12,15:17,22,24,28)],class)
+require(klaR)
+cluster = kmodes(rawdata_factors[,c(10:12,15:17,22,24,28)],4)
+require(cluster)
+#system.time(pam(daisy(rawdata_factors[,c(10:12,15:17,22,24,28)], metric="gower", type=list(symm=1:9)), k=4))
+#system.time(kmodes(rawdata_factors[,c(10:12,15:17,22,24,28)], modes=2))
+
+
+
+
+
+
 
 
 
